329ff5c00c1d05549461a4f3ad413da917e63b51
Michael Falk <mtfalk@gmail.com>
2023-06-30 15:11:32 -0500

    Replaced multiple indexOf checks with a single regular expression

--- responsive.js
+++ responsive.js
@@ -1,17 +1,13 @@
 var url = window.location.href;
 if (url && url.length) {
-	if (url.indexOf('psnprofiles.com/guides') >= 0 ||
-		url.indexOf('psnprofiles.com/guide') >= 0 ||
-		url.indexOf('psnprofiles.com/trophies') >= 0 ||
-		url.indexOf('psnprofiles.com/trophy')) {
+    if (url.match(/https?:\/\/(www\.)?psnprofiles\.com\/((guides)|(guide)|(trophies)|(trophy))/gi)) {
+        // meta viewport tag
+        var metaTag=document.createElement('meta');
+        metaTag.name = "viewport"
+        metaTag.content = "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+        document.getElementsByTagName('head')[0].appendChild(metaTag);
 
-		// meta viewport tag
-		var metaTag=document.createElement('meta');
-		metaTag.name = "viewport"
-		metaTag.content = "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
-		document.getElementsByTagName('head')[0].appendChild(metaTag);
-
-		// responsive body class
-		document.body.classList.add("responsive");
-	}
+        // responsive body class
+        document.body.classList.add("responsive");
+    }
 }
